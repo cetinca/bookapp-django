180070abde9989caf93c14f72dc986560d141350
Cetin CAKIR <cetincakirtr@gmail.com>
2022-11-24 09:13:00 +0300

    Daily commit

--- book_app/settings.py
+++ book_app/settings.py
@@ -117,8 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 # Following settings only make sense on production and may break development environments.
