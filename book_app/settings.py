940389365238ade082548b1e32629380bf1d476f
Cetin CAKIR <cetincakirtr@gmail.com>
2022-11-24 08:17:05 +0300

    Daily commit

--- book_app/settings.py
+++ book_app/settings.py
@@ -85,10 +85,13 @@ WSGI_APPLICATION = 'book_app.wsgi.application'
 # DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
 }
+
+DATABASES['default'] = dj_database_url.config()
 
 AUTH_PASSWORD_VALIDATORS = [
     {
