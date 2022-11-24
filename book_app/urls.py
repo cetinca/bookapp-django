d368992326f56a57c9ee722566505a2db64aa516
Cetin CAKIR <cetincakirtr@gmail.com>
2022-11-24 12:50:41 +0300

    Daily commit

--- book_app/urls.py
+++ book_app/urls.py
@@ -27,10 +27,11 @@ urlpatterns = [
 ]
 
 if settings.RENDER:
-    urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', serve, {
-            'document_root': settings.MEDIA_ROOT,
-        }),
-    ]
+    # urlpatterns += [
+    #     re_path(r'^media/(?P<path>.*)$', serve, {
+    #         'document_root': settings.MEDIA_ROOT,
+    #     }),
+    # ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
