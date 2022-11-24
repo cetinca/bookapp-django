912adfad48ab20c658d00d6228ef2d62764b26e6
Cetin CAKIR <cetincakirtr@gmail.com>
2022-11-24 07:48:30 +0300

    Daily commit

--- book_app/settings.py
+++ book_app/settings.py
@@ -92,9 +92,7 @@ if True if DEBUG == "True" else False:
     }
 else:
     DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600)
+        'default': dj_database_url.config()
     }
 
 AUTH_PASSWORD_VALIDATORS = [
