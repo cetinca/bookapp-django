0a7336c70534339d03acbd4ff716f5596a106a8e
Cetin CAKIR <cetincakirtr@gmail.com>
2022-11-24 09:46:57 +0300

    Daily commit

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py createsuperuser --noinput \
---username $DJANGO_SUPERUSER_USERNAME \
---email $DJANGO_SUPERUSER_EMAIL
+#python manage.py createsuperuser --noinput \
+#--username $DJANGO_SUPERUSER_USERNAME \
+#--email $DJANGO_SUPERUSER_EMAIL
